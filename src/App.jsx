76df4c856d7d9b4c64fdf7292c6a12d396169ddb
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-02 16:56:37 +0100

    jogo-adivinhacao

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Jogo from './components/Jogo'
+
+function App() {
+
+
+  return (
+    <>
+    <Jogo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Jogo.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function Jogo (){
+    const [numeroEscolhido, setNumeroEscolhido]=useState("")
+    const [buttonClick, setbuttonClick] = useState("")
+    
+    const [buttonSorteio, setButtonSorteio] = useState ("")
+
+    const handleInputChange = (ev) =>{
+        setNumeroEscolhido(ev.target.value)
+    }
+    const handleButtonClick = ()=>{
+        setbuttonClick(numeroEscolhido)
+    }
+    
+    function clickSorteio (){
+        const numero = Math.floor(Math.random() * 21)
+        setButtonSorteio(numero)
+    }
+
+    return(
+        <>
+        <h1>Jogo de Adivinhação de números</h1>
+        <h2>Escolha um número de 1 á 20</h2>
+        <div className="button-input" >
+        <input
+        type="number"
+        onChange={handleInputChange}/>
+        <button
+        onClick={handleButtonClick}
+        >Click para salvar o número</button>
+        </div>
+        <p>número escolhido: {buttonClick}</p>
+        
+        <button
+        onClick={clickSorteio}
+        >Click para sortear o número</button>
+        {buttonSorteio &&
+        <p>Número sorteado: {buttonSorteio} </p>
+       } </>
+    )
+}
